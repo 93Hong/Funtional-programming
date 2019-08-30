40b3a49a023f9f656701b0ff288aa0e72a6416b8
93hong <sk2rldnr@gmail.com>
2019-08-30 11:06:48 +0900

    - predicate with method reference - predicate with lambda

--- src/test/java/com/giwook/study/functional/stream/JavaStreamTest.java
+++ src/test/java/com/giwook/study/functional/stream/JavaStreamTest.java
@@ -3,7 +3,6 @@ package com.giwook.study.functional.stream;
 import com.google.common.collect.Lists;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.List;
 
@@ -18,7 +17,7 @@ class JavaStreamTest {
 
 	JavaStream javaStream = new JavaStream();
 
-	@DisplayName("Predicate > filterByStartWith")
+	@DisplayName("Predicate startWith with method reference")
 	@Test
 	void test_filterByStartWith() {
 
@@ -35,6 +34,28 @@ class JavaStreamTest {
 		assertThat(startWithAList).size().isEqualTo(2);
 
 		List<String> startWithBList = javaStream.filterByStartWith(stringList, JavaStream::startWithB);
+		assertThat(startWithBList).isNotNull();
+		assertThat(startWithBList).size().isEqualTo(3);
+
+	}
+
+	@DisplayName("Predicate startWith with lambda")
+	@Test
+	void test_predicate_withLambda() {
+
+		List<String> stringList = Lists.newArrayList();
+		stringList.add("Aaa");
+		stringList.add("Aaa");
+		stringList.add("Baa");
+		stringList.add("Baa");
+		stringList.add("Baa");
+		stringList.add("Caa");
+
+		List<String> startWithAList = javaStream.filterByStartWith(stringList, s -> s.startsWith("A"));
+		assertThat(startWithAList).isNotNull();
+		assertThat(startWithAList).size().isEqualTo(2);
+
+		List<String> startWithBList = javaStream.filterByStartWith(stringList, s -> s.startsWith("B"));
 		assertThat(startWithBList).isNotNull();
 		assertThat(startWithBList).size().isEqualTo(3);
 
