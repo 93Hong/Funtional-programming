dbe991bd8879c626fb4ed7b50530d701fbd40e20
93hong <sk2rldnr@gmail.com>
2019-08-30 15:41:37 +0900

    - 5 expression of lambda with java8 interface

--- src/main/java/com/giwook/study/functional/stream/Lambda.java
+++ src/main/java/com/giwook/study/functional/stream/Lambda.java
@@ -4,7 +4,9 @@ import com.google.common.base.Function;
 import com.google.common.base.Supplier;
 import com.google.common.collect.Maps;
 
+import java.util.function.BiFunction;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 /**
  *
@@ -15,12 +17,12 @@ public class Lambda {
 
 	public void lambdaExpression() {
 		Function<String, Integer> stringIntegerFunction = (String s) -> s.length();
-		Function<String, Boolean> stringBooleanFunction = (String s) -> s.length() > 3;
+		Predicate<String> p = (String s) -> s.length() > 3;
 		Consumer<String> stringConsumer = (String s) -> {
 			System.out.println(s);
 		};
 		Supplier<String> stringSupplier = () -> "string";
-		Maps.EntryTransformer<String, String, String> stringStringStringEntryTransformer = (String s1, String s2) -> s1.concat(s2);
+		BiFunction<String, String, String> stringStringStringEntryTransformer = (String s1, String s2) -> s1.concat(s2);
 	}
 
 }
